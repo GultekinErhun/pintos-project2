04ebae3aa8e6b07a47b64423763008082cd2cd07
Erhun Gültekin <160606644+GultekinErhun@users.noreply.github.com>
2024-05-04 12:39:03 +0300

    Update ipc.h

--- userprog/ipc.h
+++ userprog/ipc.h
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 typedef int pid_t;
 
-void ipc_init(void);
-int ipc_pipe_read(char *pipe_name, int ticket);
-void ipc_pipe_write(char *pipe_name, int ticket, int msg);
+void ipc_initialiser(void);
+int ipc_pipe_lecture(char *pipe_name, int ticket);
+void ipc_pipe_ecriture(char *pipe_name, int ticket, int msg);
